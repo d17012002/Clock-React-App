fdfe3038b76cc50c253d0b8479bb21ee679ca2f2
Anurag Kumar <2002anuragksingh@gmail.com>
2022-03-09 22:15:39 +0530

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import Footer from "./Footer"
+
+function App() {
+  const [time, updateTime] = useState(new Date().toLocaleTimeString());
+  console.log(time);
+  
+  function update(){
+    updateTime(new Date().toLocaleTimeString());
+  }
+
+  setTimeout(update , 1000);
+  const d = new Date();
+  let date = d.getDate();
+  let month = d.getMonth();
+  let year = d.getFullYear();
+
+  console.log(year);
+
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <h2>Date: {date}/{month+1}/{year}</h2>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer() {
+  const d = new Date();
+  return (
+    <footer>
+      
+      <p>Made with ♥ Anurag Singh</p>
+    </footer>
+  );
+}
+
+export default Footer ;
